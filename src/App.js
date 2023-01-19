b8a18146c62ab497220c704a554dcf681013133c
Melanie <mtojong@live.unc.edu.email>
2023-01-19 06:37:29 -0500

    updated sign in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component.jsx';
 import Home from './routes/home/home.component.jsx';
-import SignIn from './routes/sign-in/sign-in.components.jsx';
+import Authentication from './routes/authentication/authentication.components.jsx';
 
 const Shop = () => {
   return <h1>I am the shop</h1>;
@@ -13,7 +13,7 @@ const App = () => {
     <Route path='/' element={<Navigation />}>
       <Route index element={<Home />} />
       <Route path='shop' element={<Shop />} />
-      <Route path='sign-in' element={<SignIn />} />
+      <Route path='auth' element={<Authentication />} />
     </Route>
   </Routes>
   )
